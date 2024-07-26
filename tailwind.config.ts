d315b3b5628ffe6d02dce5779a26f89b56f907e2
julianastahelin <julianastahelin@gmail.com>
2024-07-26 15:14:30 -0300

    feat: config global variables and tailwind extended classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        primary: {
+          DEFAULT: 'var(--background-primary)',
+          foreground: 'var(--foreground-primary)',
+          accent: 'var(--accent-primary)'
+        },
+        secondary: {
+          foreground: 'var(--foreground-secondary)'
+        },
+        border: {
+          primary: 'var(--border-primary)'
+        }
+      }
     },
   },
   plugins: [],
